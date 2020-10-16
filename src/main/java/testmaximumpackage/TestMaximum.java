dbd7c3ed99e9f667a7aafcf039a5d9cc10c0428b
AV-NASH <avinash21213434@gmail.com>
2020-10-16 18:26:29 +0530

    Added method to find maximum of three Integers with test cases

--- src/main/java/testmaximumpackage/TestMaximum.java
+++ src/main/java/testmaximumpackage/TestMaximum.java
@@ -3,5 +3,14 @@ package testmaximumpackage;
 public class TestMaximum {
     public static void main(String[] args) {
         System.out.println("Welcome to test maximum program");
+        TestMaximum testMaximum=new TestMaximum();
+        System.out.println("Max of 3 number is.. "+testMaximum.findMax(12,45,67));
+    }
+    public  Integer findMax(Integer number1, Integer number2, Integer number3) {
+        Integer max=number1;
+        if(max.compareTo(number2)<0) max=number2;
+        if(max.compareTo(number3)<0) max=number3;
+
+        return max;
     }
 }
--- src/test/java/testmaximumpackage/TestMaximumTest.java
+++ src/test/java/testmaximumpackage/TestMaximumTest.java
@@ -6,6 +6,27 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestMaximumTest {
+    @Test
+    public void whenMaxIntAt_FirstPostion_ReturnMax() {
+        TestMaximum testMaximum=new TestMaximum();
+        Integer max, a=6,b=3,c=1;
+        max=a;
+        Assert.assertEquals(max,testMaximum.findMax(a,b,c));
+    }
 
+    @Test
+    public void whenMaxIntAt_SecondPostion_ReturnMax() {
+        TestMaximum testMaximum=new TestMaximum();
+        Integer max,a=2,b=3,c=1;
+        max=b;
+        Assert.assertEquals(max,testMaximum.findMax(a,b,c));
+    }
+
+    @Test
+    public void whenMaxIntAt_ThirdPostion_ReturnMax() {
+        TestMaximum testMaximum=new TestMaximum();
+        Integer max, a=6,b=3,c=8;        max=c;
+        Assert.assertEquals(max,testMaximum.findMax(a,b,c));
+    }
 
 }
